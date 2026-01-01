443dc1d9d86ad94b89c6fbcd78ef9e819f4783ed
saidbabaiev <babaiev.info@gmail.com>
2026-01-01 17:39:35 +0100

    install and setup tailwind and shadcn vue

---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex gap-4">
+    <Button>Click me</Button>
+    <Button variant="secondary">Click me</Button>
+    <Button variant="outline">Click me</Button>
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,5 +8,18 @@ export default defineNuxtConfig({
       supabaseUrl: process.env.SUPABASE_URL || '',
       supabaseKey: process.env.SUPABASE_KEY || ''
     }
+  },
+
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+
+  shadcn: {
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * Will respect the Nuxt aliases.
+     * @link https://nuxt.com/docs/api/nuxt-config#alias
+     * @default "@/components/ui"
+     */
+    componentDir: '@/components/ui'
   }
 })
