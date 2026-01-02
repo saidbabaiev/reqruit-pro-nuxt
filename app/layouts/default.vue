45757814006f72458cb2f90fd1a912c459e1b877
saidbabaiev <babaiev.info@gmail.com>
2026-01-03 00:08:12 +0100

    add shadcn sidebar with icons, profile section and second menu section

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,11 +1,50 @@
+<script setup lang="ts">
+import AppSidebar from "@/components/AppSidebar.vue"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+
+</script>
+
 <template>
   <div class="flex h-screen bg-gray-100">
-    <aside class="w-64 border-r p-4">
-       Sidebar
-    </aside>
-
-    <main class="flex-1 p-8 overflow-auto">
-      <slot />
-    </main>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header class="flex h-16 shrink-0 items-center gap-2">
+          <div class="flex items-center gap-2 px-4">
+            <SidebarTrigger class="-ml-1" />
+            <Separator orientation="vertical" class="mr-2 data-[orientation=vertical]:h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem class="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Building Your Application
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator class="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <main class="flex-1 p-8 overflow-auto">
+          <slot />
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
   </div>
 </template>
